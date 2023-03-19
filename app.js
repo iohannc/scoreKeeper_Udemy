59210886096e65f1c025e4b25c20925e447a0a93
Juan Negrete <juan.negrete.dev@gmail.com>
2023-03-19 17:35:27 -0600

    add html select element to let user pick a score limit, change score colors using classes dependind on which one wins

--- app.js
+++ app.js
@@ -4,25 +4,52 @@ const playerTwoScore = document.querySelector("#pl2-score");
 const playerOneScoreButton = document.querySelector("#pl1-scorebutton");
 const playerTwoScoreButton = document.querySelector("#pl2-scorebutton");
 const resetButton = document.querySelector("#resetbutton");
+const scoreLimit = document.querySelector("#score-limit");
+
+// Set score limit
+scoreLimit.value = 5; // Resets value with each reload.
+let scoreLimitValue = scoreLimit.value;
+scoreLimit.addEventListener("input", () => {
+  scoreLimitValue = scoreLimit.value;
+  resetButton.click();
+});
 
 // Linking buttons to score board
 playerOneScoreButton.addEventListener("click", () => {
-  let currentScore = playerOneScore.innerText;
-  if (currentScore !== "5") {
+  let currentScore = playerOneScore.textContent;
+  if (currentScore != scoreLimitValue) {
     let newScore = Number(currentScore) + 1;
-    playerOneScore.innerText = newScore;
+    playerOneScore.textContent = newScore;
+  }
+  if (playerOneScore.textContent == scoreLimitValue) {
+    playerOneScore.classList.add("winner");
+    playerTwoScore.classList.add("loser");
   }
 });
 
 playerTwoScoreButton.addEventListener("click", () => {
-  let currentScore = playerTwoScore.innerText;
-  if (currentScore !== "5") {
+  let currentScore = playerTwoScore.textContent;
+  if (currentScore != scoreLimitValue) {
     let newScore = Number(currentScore) + 1;
-    playerTwoScore.innerText = newScore;
+    playerTwoScore.textContent = newScore;
+  }
+  if (playerTwoScore.textContent == scoreLimitValue) {
+    playerTwoScore.classList.add("winner");
+    playerOneScore.classList.add("loser");
   }
 });
 
 resetButton.addEventListener("click", () => {
   playerOneScore.innerText = "0";
   playerTwoScore.innerText = "0";
+  if (playerOneScore.classList.contains("winner")) {
+    playerOneScore.classList.remove("winner");
+  } else if (playerOneScore.classList.contains("loser")) {
+    playerOneScore.classList.remove("loser");
+  }
+  if (playerTwoScore.classList.contains("winner")) {
+    playerTwoScore.classList.remove("winner");
+  } else if (playerTwoScore.classList.contains("loser")) {
+    playerTwoScore.classList.remove("loser");
+  }
 });
